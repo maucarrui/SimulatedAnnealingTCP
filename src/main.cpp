4d2afb96a8eef25afa3639bd787eee32edca3685
maucarrui <maucarrui@ciencias.unam.mx>
2020-10-21 22:52:39 -0500

    Main: Successfully reading from database.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,18 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <map>
 
+#include "DAO.h"
+
+/**
+ * Parses the current line, separating each substring
+ * delimited by a comma and casting it into an int, 
+ * finally pushing it into the vector.
+ *
+ * @param IDs  The current IDs that have been read.
+ * @param line The line containing the IDs.
+ */
 void parseLine(std::vector<int> &IDs, std::string line) {
     std::stringstream s_stream(line);;
     
@@ -15,6 +26,13 @@ void parseLine(std::vector<int> &IDs, std::string line) {
 
 }
 
+/**
+ * Gets the IDs of the cities given in the file passed by
+ * argument.
+ *
+ * @param  fileName The name of the file containing the IDs.
+ * @return A vector containing all of the IDs in the file.
+ */
 std::vector<int> getIDs(std::string fileName) {
      std::vector<int> IDs;
      std::string line;
@@ -32,6 +50,8 @@ std::vector<int> getIDs(std::string fileName) {
 }
 
 int main(int argc, char** argv) {
+
+    // Handle too few or too many arguments.
     if (argc < 2) {
         std::cout << "Missing argument: Cities file required.\n";
 	return 0;
@@ -41,9 +61,36 @@ int main(int argc, char** argv) {
     }
 
     std::string citiesFile = argv[1];
-    std::string pathDB     = "sql/database.db";
+    const char* pathDB     = "sql/database.db";
 
+    // Get the IDs from the file.
     std::vector<int> IDs = getIDs(citiesFile);
+
+    // Get the cities.
+    DAO dao;
+    std::map<int, City> cities;
+
+    dao.openDB(pathDB);
+
+    int i = 0;
+    for(int j : IDs) {
+        cities.emplace(i, dao.getCityByID(j));
+	i++;
+    }
+
+    std::map<int, City>::iterator it;
+    for(it = cities.begin(); it != cities.end(); it++){
+        std::cout << "("
+	          << it->first
+	          << ";"
+	          << it->second.getName()
+	          << ";"
+	          << it->second.getID()
+	          << ")"
+	          << std::endl;
+    }
+
+    dao.closeDB();
 
     return 0;
 }

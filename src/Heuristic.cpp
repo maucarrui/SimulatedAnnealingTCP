ca91e11b347041b2e3faffacc75c203cb3334ce7
maucarrui <maucarrui@ciencias.unam.mx>
2020-10-28 15:26:48 -0600

    Heuristics methods added and compiles without errors.

--- src/Heuristic.cpp
+++ src/Heuristic.cpp
@@ -1,33 +1,131 @@
-#include "Hueristic.h"
+#include "Heuristic.h"
 
-Heurisic::Heuristic(Graph G, Solution initialSolution, 
-		    double initialTemperature, double coolingFactor) {
+Heuristic::Heuristic(Graph G, Solution initialSolution, 
+		    double initialTemperature, double coolingFactor,
+		    double L, double epsilon, double epsilon_p) {
     this->G                = G;
+    this->L                = L;
     this->currentSolution  = initialSolution;
     this->temperature      = initialTemperature;
     this->coolingFactor    = coolingFactor;
+    this->epsilon          = epsilon;
+    this->epsilon_p        = epsilon_p;
 }
 
 std::pair<double, Solution> Heuristic::calculateBatch(double T, Solution s) {
-    return std::make_pair(T, s);
+    int c    = 0;
+    double r = 0.0;
+    Solution current = s;
+    Solution neighbor;
+    double currentCost, newCost;
+    
+    while (c < L) {
+        neighbor    = s.getRandomNeighbor();
+	currentCost = G.getCost(current);
+	newCost     = G.getCost(neighbor);
+
+	if (newCost <= currentCost + T) {
+	    current = neighbor;
+	    c++;
+	    r += newCost;
+	}
+    }
+    
+    return std::make_pair((r / L), current);
 }
 
-void Hueristic::thresholdAcceptance(double T, Solution s) {
-    return;
+void Heuristic::thresholdAcceptance() {
+    double p = 0.0, q = 0.0;
+    std::pair<double, Solution> ps;
+    
+    while (temperature > epsilon) {
+        q = std::numeric_limits<double>::infinity();
+	
+	while (p <= q) { 
+	    q = p;
+	    ps = calculateBatch(temperature, currentSolution);
+	    q = ps.first;
+	    currentSolution = ps.second;
+	}
+
+	temperature *= coolingFactor;
+    }
+
 }
 
-double Hueristic::getInitialTemperature(Solution s, double T, double P) {
-    return T;
+double Heuristic::getInitialTemperature(Solution s, double T, double P) {
+    double T1, T2;
+    double p = acceptedPercentage(s, T);
+
+    if (std::abs((P - p)) <= epsilon_p) {
+        return T;
+    }
+    
+    if (p < P) {
+
+        while (p < P) {
+	    T = 2 * T;
+	    p = acceptedPercentage(s, T);
+	}
+
+	T1 = T / 2;
+	T2 = T;
+
+    } else {
+
+        while (p > P) {
+	    T = T / 2;
+	    p = acceptedPercentage(s, T);
+	}
+	
+	T1 = T;
+	T2 = 2 * T;
+
+    }
+
+    return binarySearch(s, T1, T2, P);
 }
 
 double Heuristic::acceptedPercentage(Solution s, double T) {
-    return T;
+    double c = 0.0;
+    Solution newSolution;
+    Solution current = s;
+
+    double currentCost, newCost;
+    
+    for (int i = 0; i < L; i++) {
+        newSolution = current.getRandomNeighbor();
+	currentCost = G.getCost(current);
+	newCost     = G.getCost(newSolution);
+	
+	if (newCost <= currentCost + T) {
+	    c++;
+	    current = newSolution;
+	}
+    }
+
+    return c / L;
 }
 
 double Heuristic::binarySearch(Solution s, double T1, double T2, double P) {
+    double Tm = (T1 + T2) / 2;
+    
+    if ( (T2 - T1) < epsilon_p )
+        return Tm;
+    
+    double p = acceptedPercentage(s, Tm);
+
+    if (std::abs((P-p)) < epsilon_p)
+      return Tm;
+
+    if (p > P)
+      return binarySearch(s, T1, Tm, P);
+    else
+      return binarySearch(s, Tm, T2, P);
+    
     return T1;
 }
 
-Solution Heuristic::getCurrentSolution {
+Solution Heuristic::getCurrentSolution() {
     return currentSolution;
 }
--- src/Heuristic.h
+++ src/Heuristic.h
@@ -1,4 +1,5 @@
-#include <pair>
+#include <utility>
+#include <limits>
 
 #ifndef SOLUTION_H
 #define SOLUTION_H
@@ -13,21 +14,27 @@
 class Heuristic {
     private:
         Graph G;
-        Solution currenSolution;
+        Solution currentSolution;
 	double temperature;
 	double coolingFactor;
+        double L;
+        double epsilon;
+        double epsilon_p;
 
     public:
-	Hueristic(Graph G, 
+	Heuristic(Graph G, 
 		  Solution initialSolution, 
 		  double initialTemperature,
-		  double coolingFactor);
+		  double coolingFactor,
+		  double L,
+		  double epislon,
+		  double epsilon_p);
 
 	std::pair<double, Solution> calculateBatch(double T, Solution s);
-	void thresholdAcceptance(double T, Solution s);
+	void thresholdAcceptance();
 	double getInitialTemperature(Solution s, double T, double P);
 	double acceptedPercentage(Solution s, double T);
 	double binarySearch(Solution s, double T1, double T2, double P);
 
 	Solution getCurrentSolution();
-}
+};
